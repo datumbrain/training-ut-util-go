c11c3fc00bc5e9fc396bc5553d0b4f2fee100380
Usman Tahir <utahir243@gmail.com>
2024-07-05 17:25:01 +0500

    Merge Util Tool  * Added util.go file with conversion logic  * Added input directory with dummy jsonl files  * Added scripts directory with the relevant scripts  * Docker file and .yml file with docker configuration logic  * Added Output directory to store dockers conversion output  * Edited Readme with instructions on how to run the tool  * Added the .gitignore file

---
+++ scripts/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+usage(){
+    echo -e "Press 1 to merge the JSONL files into a separate file\n"
+    echo -e "Press 2 to merge the JSONL files into a CSV\n"
+}
+
+operation=""
+input_path=""
+output_path=""
+format="jsonl"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --operation)
+            operation="$2"
+            shift 2  # Move past the argument and its value
+            ;;
+        --input-path)
+            input_path="$2"
+            shift 2
+            ;;
+        --output-path)
+            output_path="$2"
+            shift 2
+            ;;
+        --format)
+            format="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid option: $1"
+            ;;
+    esac
+done
+
+if [ "$1" == "-b" ]; then
+    cd ..
+    mkdir -p bin
+    go build -o bin/util ./util.go
+else
+    usage
+    read -r input
+    if [ "$input" == 1 ]; then
+        cd ..
+        go run util.go --operation merge --input-path "$input_path" --output-path "$output_path"
+    elif [ "$input" == 2 ]; then
+        cd ..
+        go run util.go --operation merge --input-path "$input_path" --output-path "$output_path" --format csv
+    fi
+fi
